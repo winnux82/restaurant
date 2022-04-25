f8fe1ec30208b70245a63510c78811028c5374d4
winnux82 <vandermeulen.christophe@gmail.com>
2022-04-25 13:53:45 +0200

    d

--- customer.php
+++ customer.php
@@ -16,3 +16,12 @@ public function get_id()
 {
     return $this -> _id;
 }
+
+/*
+https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_animation2
+
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+  
